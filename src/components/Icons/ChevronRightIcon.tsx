86aa65d532e84112aa28572bd1a3d9c20f8aeb8b
chandan <chandankl0310@gmail.com>
2023-07-20 02:10:52 +0530

    feat: Add Icon Components

---
+++ src/components/Icons/ChevronRightIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function ChevronRightIcon() {
+  return (
+<svg className="chevron-right" viewBox="0 0 12 12" width="12" height="12" fill="currentColor" style={{color: 'rgb(125, 133, 144)'}}><path d="M4.7 10c-.2 0-.4-.1-.5-.2-.3-.3-.3-.8 0-1.1L6.9 6 4.2 3.3c-.3-.3-.3-.8 0-1.1.3-.3.8-.3 1.1 0l3.3 3.2c.3.3.3.8 0 1.1L5.3 9.7c-.2.2-.4.3-.6.3Z"></path></svg>  )
+}
+
+export default ChevronRightIcon
